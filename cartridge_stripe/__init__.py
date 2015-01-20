13e83d0cff863c2bac3717ac20da82e166ebab41
smoothstonedesign <brent.arnold@gmail.com>
2015-01-20 15:57:26 -0500

    decimal to cents fixed
    fixed the order of operations for stripe.Charge

--- cartridge_stripe/__init__.py
+++ cartridge_stripe/__init__.py
@@ -28,7 +28,7 @@ def payment_handler(request, order_form, order):
     tok = order_form.cleaned_data['stripe_token']
     total = order.total
     try:
-        charge = stripe.Charge.create(amount=int(total) * 100,
+        charge = stripe.Charge.create(amount=int(total * 100),
                                       currency="usd",
                                       card=tok,
                                       description=order)
